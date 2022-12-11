3590bca7876883f37b27e9116eb55322ecf21cb0
aslnabdullah <abdullahaslan2510@gmail.com>
2022-12-11 15:52:08 +0300

    apo

---
+++ AnkaHUJAM/Assets/Scripts/Abdullah/BeforeMenuScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeforeMenuScript : MonoBehaviour
+{
+    public GameObject BeforeMenuCanvas;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Menu", LoadSceneMode.Additive);
+            BeforeMenuCanvas.SetActive(false);
+
+        }
+        
+    }
+}
